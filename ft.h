9e1954fe1b4ae99c252f87bfc197df161b0e2b2f
Chakib-C <64082927+Chakib-C@users.noreply.github.com>
2022-07-24 17:18:54 +0200

    Add files via upload

---
+++ ft.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cchouire <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/23 17:50:08 by cchouire          #+#    #+#             */
+/*   Updated: 2022/07/23 17:51:06 by cchouire         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+# include <unistd.h>
+# include <stdlib.h>
+
+int		ft_strlen(char *str);
+void	ft_putchar(char c);
+void	ft_putstr(char *str);
+
+#endif
